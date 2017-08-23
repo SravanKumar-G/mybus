eb68562e9eacdb50b70cc20ea3b089e6b622b6e1
srinikandula <srini.kandula@shodogg.com>
2017-08-23 09:06:52 +0530

    Fixing service expense API

--- src/main/java/com/mybus/dao/ServiceExpenseDAO.java
+++ src/main/java/com/mybus/dao/ServiceExpenseDAO.java
@@ -5,10 +5,10 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface ServiceExpenseDAO extends PagingAndSortingRepository<ServiceExpense, String> {
-    Iterable<ServiceExpense> findByJourneyDate(Date date);
-    ServiceExpense findByJourneyDateAndServiceNumber(Date date, String serviceNUmber);
+    List<ServiceExpense> findByJourneyDate(Date date);
 
 }
