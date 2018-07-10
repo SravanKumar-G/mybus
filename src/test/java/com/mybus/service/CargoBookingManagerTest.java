5d0068c6aa3a3d2e506f89d10f448009939bfdd3
srinikandula <srini.kandula@shodogg.com>
2018-07-10 10:48:28 +0530

    Fixing the test case for CargoBooking

--- src/test/java/com/mybus/service/CargoBookingManagerTest.java
+++ src/test/java/com/mybus/service/CargoBookingManagerTest.java
@@ -5,6 +5,7 @@ import com.mybus.dao.CargoBookingDAO;
 import com.mybus.dao.cargo.ShipmentSequenceDAO;
 import com.mybus.exception.BadRequestException;
 import com.mybus.model.CargoBooking;
+import com.mybus.model.PaymentStatus;
 import com.mybus.model.cargo.ShipmentSequence;
 import com.mybus.test.util.CargoBookingTestService;
 import org.apache.commons.collections.IteratorUtils;
