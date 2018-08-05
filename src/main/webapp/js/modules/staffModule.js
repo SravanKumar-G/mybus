a56804cd959ed739eea0b6855ddc33baf34a5708
srinikandula <srini.kandula@shodogg.com>
2018-08-05 17:31:17 +0530

    Fixing staff list screen

--- src/main/webapp/js/modules/staffModule.js
+++ src/main/webapp/js/modules/staffModule.js
@@ -56,11 +56,11 @@ angular.module('myBus.staffModule', ['ngTable', 'ui.bootstrap'])
     .controller('EditStaffController', function ($scope,$state,$stateParams, $rootScope, $http,$log, $location, staffManager) {
         $scope.headline = "Staff";
         $scope.staffId = $stateParams.id;
-        $scope.allStaff = {};
+        $scope.staff = {};
         console.log('staffId ' + $scope.staffId );
         if($scope.staffId) {
             staffManager.getStaff($scope.staffId, function(response){
-                $scope.allStaff = response;
+                $scope.staff = response;
             }, function(error){
                 sweetAlert("Error finding staff",err.data.message,"error");
             });
