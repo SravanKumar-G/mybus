262e3708b914dee806a3b9f0cfb03c0ede1aa6a4
Sravan <sravan.g@mtwlabs.com>
2018-10-10 13:51:16 +0530

    added search functionality in loadingSheet

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -35,7 +35,9 @@ var myBus = angular.module('myBus', [
     'myBus.operatorAccountsModule',
     'myBus.staffModule',
     'myBus.cargoDashboard',
-    'myBus.cargoBranchSummary'
+    'myBus.cargoBranchSummary',
+    'ui.select',
+    'ngSanitize'
 ]);
 
 myBus.config(['$stateProvider','$urlRouterProvider',
@@ -518,4 +520,35 @@ myBus.run(function ($rootScope,$state, $location, appConfigManager, userManager,
         }
     });
 
+});
+myBus.filter('propsFilter', function() {
+    return function(items, props) {
+        var out = [];
+
+        if (angular.isArray(items)) {
+            var keys = Object.keys(props);
+
+            items.forEach(function(item) {
+                var itemMatches = false;
+
+                for (var i = 0; i < keys.length; i++) {
+                    var prop = keys[i];
+                    var text = props[prop].toLowerCase();
+                    if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+                        itemMatches = true;
+                        break;
+                    }
+                }
+
+                if (itemMatches) {
+                    out.push(item);
+                }
+            });
+        } else {
+            // Let the output be the input untouched
+            out = items;
+        }
+
+        return out;
+    };
 });
